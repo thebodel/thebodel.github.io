81c3b146eeb9f964a813a98a0f9a05d22ce184c5
Sukhina Bohdan <bogdansuhina14@gmail.com>
2026-05-09 20:30:06 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const messages = [
-    { type: "text", content: "Привіт, нажми пробел" },
+    { type: "text", content: "Првіт, нажми пробел" },
     { type: "image", content: "img/privit.jpg" },
 ];
 
